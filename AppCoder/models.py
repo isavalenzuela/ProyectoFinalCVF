02e8a12d97da115e1e28831589063c9cb4235a42
isavalenzuela <isadoravalenzuelafierro@gmail.com>
2022-05-03 23:31:49 -0400

    this is just an update
    so I can save the changes

---
+++ AppCoder/models.py
@@ -0,0 +1,8 @@
+from statistics import mode
+from django.db import models
+
+# Create your models here.
+
+class Curso(models.Model):
+    nombre=models.CharField(max_length=50)
+    camada=models.IntegerField()
